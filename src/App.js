69f9cc09ae8fe91f1ca1e1f4e89424e0710b3a9d
AndrewChacon <andrew.b.chacon@gmail.com>
2022-03-18 07:27:42 -0400

    feedback list component, managing global state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FeedbackItem from './components/FeedbackItem';
+import FeedbackList from './components/FeedbackList';
 import Header from './components/Header';
+import FeedbackData from './data/FeedbackData';
 
 function App() {
+	const [feedback, setFeedback] = useState(FeedbackData);
 	return (
 		<>
 			<Header />
 			<div className='container'>
 				<h1>My App</h1>
-				<FeedbackItem />
+				<FeedbackList feedback={feedback} />
 			</div>
 		</>
 	);
