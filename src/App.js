62809850ef2f8adff9c57cb41ae50bd45080333a
AndrewChacon <andrew.b.chacon@gmail.com>
2022-03-19 05:50:21 -0400

    events and prop drilling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { useState } from 'react';
-import FeedbackItem from './components/FeedbackItem';
+// import FeedbackItem from './components/FeedbackItem';
 import FeedbackList from './components/FeedbackList';
 import Header from './components/Header';
 import FeedbackData from './data/FeedbackData';
 
 function App() {
 	const [feedback, setFeedback] = useState(FeedbackData);
+
+	const deleteFeedback = id => {
+		if (window.confirm('Are you sure if you want to delete?')) {
+			setFeedback(feedback.filter(item => item.id !== id));
+		}
+	};
+
 	return (
 		<>
 			<Header />
 			<div className='container'>
 				<h1>My App</h1>
-				<FeedbackList feedback={feedback} />
+				<FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
 			</div>
 		</>
 	);
