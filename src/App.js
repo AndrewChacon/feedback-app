2e4b1509c0414059ec6877cc7e07379dac6222cc
AndrewChacon <andrew.b.chacon@gmail.com>
2022-03-23 09:11:21 -0400

    user navigation, creating routes and links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import FeedbackForm from './components/FeedbackForm';
-// import FeedbackItem from './components/FeedbackItem';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import Header from './components/Header';
 import FeedbackData from './data/FeedbackData';
 import { v4 as uuidv4 } from 'uuid';
+import AboutPage from './pages/AboutPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AboutIconLink from './components/AboutIconLink';
 
 function App() {
 	const [feedback, setFeedback] = useState(FeedbackData);
@@ -22,14 +24,30 @@ function App() {
 	};
 
 	return (
-		<>
+		<Router>
 			<Header />
 			<div className='container'>
-				<FeedbackForm handleAdd={addFeedback} />
-				<FeedbackStats feedback={feedback} />
-				<FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+				<Routes>
+					<Route
+						exact
+						path='/'
+						element={
+							<>
+								<FeedbackForm handleAdd={addFeedback} />
+								<FeedbackStats feedback={feedback} />
+								<FeedbackList
+									feedback={feedback}
+									handleDelete={deleteFeedback}
+								/>
+							</>
+						}
+					/>
+
+					<Route exact path='/about' element={<AboutPage />} />
+				</Routes>
+				<AboutIconLink />
 			</div>
-		</>
+		</Router>
 	);
 }
 
