0429ef2ebf69f0e4050793f4c91f31646b2e5a75
AndrewChacon <andrew.b.chacon@gmail.com>
2022-04-15 11:22:10 -0400

    moving feedback item functions like addfeedback and deletefeedback to our context api

--- src/App.js
+++ src/App.js
@@ -3,27 +3,12 @@ import FeedbackForm from './components/FeedbackForm';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import Header from './components/Header';
-import FeedbackData from './data/FeedbackData';
-import { v4 as uuidv4 } from 'uuid';
 import AboutPage from './pages/AboutPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AboutIconLink from './components/AboutIconLink';
 import { FeedbackProvider } from './context/FeedbackContext';
 
 function App() {
-	const [feedback, setFeedback] = useState(FeedbackData);
-
-	const addFeedback = newFeedback => {
-		newFeedback.id = uuidv4();
-		setFeedback([newFeedback, ...feedback]);
-	};
-
-	const deleteFeedback = id => {
-		if (window.confirm('Are you sure if you want to delete?')) {
-			setFeedback(feedback.filter(item => item.id !== id));
-		}
-	};
-
 	return (
 		<FeedbackProvider>
 			<Router>
@@ -35,9 +20,9 @@ function App() {
 							path='/'
 							element={
 								<>
-									<FeedbackForm handleAdd={addFeedback} />
-									<FeedbackStats feedback={feedback} />
-									<FeedbackList handleDelete={deleteFeedback} />
+									<FeedbackForm />
+									<FeedbackStats />
+									<FeedbackList />
 								</>
 							}
 						/>
